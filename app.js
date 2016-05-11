4efa1aba61c5efd4b20ab9b68aea35d2c943dfc3
grover plaza quiroga <grover.plaza@gmail.com>
2016-05-11 04:02:23 -0400

    next 28

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var User = require("./models/user").User;
+var session = require("express-session");
+var router_app = require("./routes_app");
+var session_middleware = require("./middlewares/session");
+
 var app = express();
 
-
 app.use("/estatico", express.static('public'));
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(session({
+    secret: "123bgh54ss6ed",
+    resave: false,
+    saveUninitialized: false
+}));
+// resave en true permite modificar
+// saveUninitialilized
 app.set("view engine", "jade");
 
 app.get("/", function(req,res){
-    res.render("index")
+    console.log(req.session.user_id);
+    res.render("index");
 });
-app.get("/login", function(req,res){
+app.get("/signup", function(req,res){
     User.find(function(err,doc){
         console.log(doc);
-        res.render("login");
+        res.render("signup");
     })
+});
+app.get("/login", function(req,res){
+    res.render("login");
 });
 app.post("/users", function(req, res){
     //console.log("contrase;a: "+req.body.password);
@@ -29,13 +41,22 @@ app.post("/users", function(req, res){
         password_confirmation: req.body.password_confirmation,
         username: req.body.username
                         });
-    console.log(user.password_confirmation);
-    user.save(function(err){
-        if(err){
-            console.log(String(err));
-        }
-        res.send("recibimos tus datos ok")
-    })
+    user.save().then(function(us){
+        res.send("guardamos el usuario exitosamente");
+    }, function(err){
+        console.log(String(err));
+        res.send("no pudimos guardar los datos");
+        
+    });
 });
 
+app.post("/sessions", function(req, res){
+    User.findOne({email:req.body.email, password: req.body.password}, function(err, user){
+        req.session.user_id = user._id;
+        res.send("hola mundo");
+    });
+});
+
+app.use("/app", session_middleware);
+app.use("/app", router_app);
 app.listen(80);

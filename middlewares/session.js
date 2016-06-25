82f2f216d5ad608200bdc62da011364d6725b20c
grover <grover.plaza@gmail.com>
2016-06-24 22:25:00 -0400

    finaly 28

--- middlewares/session.js
+++ middlewares/session.js
@@ -3,15 +3,14 @@ module.exports = function(req, res, next){
     if(!req.session.user_id){
         res.redirect("/login")
     }else{
-        User.findById(req.session.user_id, function(err,user){
+        User.findById(req.session.user_id, function(err, user){
             if(err){
                 console.log(err);
                 res.redirect("/login");
             }else{
-                res.locals = {user: user};
+                res.locals = { user: user };
                 next();
             }
         })
-        next();
     }
 }
